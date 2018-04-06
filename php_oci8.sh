5b98f18b9815f3cf6643d98be39fdf34a9724287
sujan <byanjankarsujan@gmail.com>
2018-04-06 12:38:14 +0545

    Update php_oci8.sh

--- php_oci8.sh
+++ php_oci8.sh
@@ -12,17 +12,17 @@ sudo mkdir -p /opt/oracle
 
 # download required packages of oracle instant client
 wget https://github.com/bumpx/oracle-instantclient/blob/master/instantclient-basic-linux-12.2.0.1.0.zip 
-wget https://github.com/bumpx/oracle-instantclient/raw/master/instantclient-sdk-linux.x64-12.1.0.2.0.zip
+wget https://github.com/bumpx/oracle-instantclient/raw/master/instantclient-sdk-linux.x64-12.2.0.1.0.zip
 
 # Unzip downlaoded package files to imstallation folder
-sudo unzip -o ./instantclient-basic-linux.x64-12.1.0.2.0.zip -d /opt/oracle
-sudo unzip -o ./instantclient-sdk-linux.x64-12.1.0.2.0.zip -d /opt/oracle
+sudo unzip -o ./instantclient-basic-linux.x64-12.2.0.1.0.zip -d /opt/oracle
+sudo unzip -o ./instantclient-sdk-linux.x64-12.2.0.1.0.zip -d /opt/oracle
 
 # create symbolic links
 sudo ln -s /opt/oracle/instantclient/sqlplus /usr/bin/sqlplus
-sudo ln -s /opt/oracle/instantclient_12_1 /opt/oracle/instantclient
-sudo ln -s /opt/oracle/instantclient/libclntsh.so.12.1 /opt/oracle/instantclient/libclntsh.so
-sudo ln -s /opt/oracle/instantclient/libocci.so.12.1 /opt/oracle/instantclient/libocci.so
+sudo ln -s /opt/oracle/instantclient_12_2 /opt/oracle/instantclient
+sudo ln -s /opt/oracle/instantclient/libclntsh.so.12.2 /opt/oracle/instantclient/libclntsh.so
+sudo ln -s /opt/oracle/instantclient/libocci.so.12.2 /opt/oracle/instantclient/libocci.so
 
 # install oci8
 echo 'instantclient,/opt/oracle/instaltclient' | sudo pecl install oci8
